bcde97cb20f68e1672773b37fea4a0bad0de86cd
The Adventurer <leiurusrint@gmail.com>
2024-09-24 09:14:49 +0500

    [First Commit] The first launch, application.yaml configured for local run: - Postgres is set up - Mongo is set up

---
+++ src/main/java/com/example/meppidoupkteiphhqnda/MeppidoupkteiphhqndaApplication.java
@@ -0,0 +1,13 @@
+package com.example.meppidoupkteiphhqnda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeppidoupkteiphhqndaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeppidoupkteiphhqndaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/meppidoupkteiphhqnda/MeppidoupkteiphhqndaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meppidoupkteiphhqnda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeppidoupkteiphhqndaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
