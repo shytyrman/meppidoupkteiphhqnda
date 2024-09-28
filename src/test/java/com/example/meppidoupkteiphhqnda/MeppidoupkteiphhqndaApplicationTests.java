b8798b59b6c16f468fbbd749c8d1068e1fd70cd7
The Adventurer <leiurusrint@gmail.com>
2024-09-28 12:02:24 +0500

    [Fix] TestNG runs in JUnit Platform alongside with Junit5

--- src/test/java/com/example/meppidoupkteiphhqnda/MeppidoupkteiphhqndaApplicationTests.java
+++ src/test/java/com/example/meppidoupkteiphhqnda/MeppidoupkteiphhqndaApplicationTests.java
@@ -1,12 +1,16 @@
 package com.example.meppidoupkteiphhqnda;
 
+import com.example.meppidoupkteiphhqnda.repository.PersonRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("local")
 class MeppidoupkteiphhqndaApplicationTests {
+
+    PersonRepository repository = Mockito.mock();
 
     @Test
     void contextLoads() {
---
+++ src/test/java/com/example/meppidoupkteiphhqnda/service/PersonCrud/BasicPersonCrudServiceTest.java
@@ -0,0 +1,31 @@
+package com.example.meppidoupkteiphhqnda.service.PersonCrud;
+
+import com.example.meppidoupkteiphhqnda.MeppidoupkteiphhqndaApplication;
+import com.example.meppidoupkteiphhqnda.repository.PersonRepository;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class BasicPersonCrudServiceTest extends AbstractTestNGSpringContextTests {
+
+    @Test
+    public void testFind() {
+    }
+
+    @Test
+    public void testDelete() {
+    }
+
+    @Test
+    public void testFindAll() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+}
