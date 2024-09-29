1b5132a3b69ec0dc004fc6a062b4a8820ec63a83
The Adventurer <leiurusrint@gmail.com>
2024-09-29 21:37:53 +0500

    [Test] Test profile set up and Postgres CRUD dummy tests written

--- src/test/java/com/example/meppidoupkteiphhqnda/MeppidoupkteiphhqndaApplicationTests.java
+++ src/test/java/com/example/meppidoupkteiphhqnda/MeppidoupkteiphhqndaApplicationTests.java
@@ -1,19 +1,28 @@
 package com.example.meppidoupkteiphhqnda;
 
+import com.example.meppidoupkteiphhqnda.model.request.Filter;
 import com.example.meppidoupkteiphhqnda.repository.PersonRepository;
+import com.example.meppidoupkteiphhqnda.service.PersonCrud.BasicPersonCrudService;
+import com.example.meppidoupkteiphhqnda.service.PersonCrud.PersonCrudService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("local")
 class MeppidoupkteiphhqndaApplicationTests {
 
-    PersonRepository repository = Mockito.mock();
+    @Autowired
+    PersonRepository repository;
 
+    @Autowired
+    PersonCrudService service;
+
+    Filter filter = new Filter(20, 20);
     @Test
     void contextLoads() {
+        service.findAll(filter);
     }
 
 }
